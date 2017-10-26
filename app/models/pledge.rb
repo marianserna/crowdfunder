ff59b1b43228f06d64ce119097188c1b357dd7f7
Marian Serna <marianhalliday@gmail.com>
2017-10-26 16:32:59 -0400

    Fixing reward tests

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -4,4 +4,11 @@ class Pledge < ApplicationRecord
 
   validates :dollar_amount, presence: true
   validates :user, presence: true
+  validate :owner_cannot_back_own_project
+
+  def owner_cannot_back_own_project
+    if self.user == self.project.user
+      errors.add(:user, 'Owner cannot pledge to their pledge')
+    end
+  end
 end
