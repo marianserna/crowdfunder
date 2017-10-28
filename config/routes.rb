a9826f11b7b66a7749900658f606933ddd00cea2
COREY COSMAN <cosman.corey@gmail.com>
2017-10-27 20:14:40 -0400

    nest comments in projects and users routes

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   resources :projects, only: [:index, :new, :create, :show] do
     resources :pledges, only: [:create]
     resources :rewards, only: [:new, :create, :destroy]
+    resources :comments, only: %i(create destroy update edit)
   end
   resources :users, only: [:new, :create, :show] do
     resources :comments, only:[:create, :edit, :update, :destroy]
