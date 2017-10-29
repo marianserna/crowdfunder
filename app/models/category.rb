8887f81e53922b3d7812f1d97e80df5e1c79a7ee
Marian Serna <marianhalliday@gmail.com>
2017-10-28 21:01:10 -0400

    Search functionality

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,9 @@
 class Category < ApplicationRecord
   belongs_to :project
+
+  def self.options_for_select
+    Category.order(:name).pluck(:name, :id)
+    # OR
+    # Category.order(:name).map { |category| [ category.name, category.id ] }
+  end
 end
