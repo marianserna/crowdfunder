723d2bc98978eff27593b918adcd21bcfe45a975
Marian Serna <marianhalliday@gmail.com>
2017-10-30 09:02:39 -0400

    Adding factory bot and testing search functionality + Fixing mistaken relationships for category model

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
   gem 'faker'
+  gem 'factory_bot_rails', '~> 4.8', '>= 4.8.2'
 end
 
 group :development do
--- test/controllers/projects_controller_test.rb
+++ test/controllers/projects_controller_test.rb
@@ -22,10 +22,35 @@ class ProjectsControllerTest < ActionDispatch::IntegrationTest
     )
   end
 
-  # test "should get index" do
-  #   get projects_url
-  #   assert_response :success
-  # end
+  # Using factory bot
+  test "should get index" do
+    category1 = create(:category)
+    category2 = create(:category)
+
+    project1 = create(:project, category: category1)
+    project2 = create(:project, category: category2)
+
+    get projects_url
+    assert_response :success
+
+    assert_equal(2, assigns(:projects).length)
+  end
+
+  test "should get search results" do
+    category1 = create(:category)
+    category2 = create(:category)
+
+    project1 = create(:project, category: category1)
+    project2 = create(:project, category: category2)
+
+    get projects_url, params: { category_id: category2.id }
+    assert_response :success
+
+    # Assigns accesses the @ variable from the controller (@projects is :projects)
+    assert_equal(1, assigns(:projects).length)
+    # raise assigns(:projects).inspect
+  end
+
   #
   # test "should get new" do
   #   get new_project_url
