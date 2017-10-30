f3c58feec23f815e64dfd9ffd6d53964b92c42aa
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-10-30 00:49:32 -0400

    carried over code from pledgee, withouht a form

--- app/controllers/claims_controller.rb
+++ app/controllers/claims_controller.rb
@@ -1,2 +1,24 @@
 class ClaimsController < ApplicationController
+  before_action :require_login
+  before_action :load_project
+
+  def create
+    @claim = @project.claims.build
+    @claim.project_id = params[:project][:id]
+    @claim.user_id = current_user
+    @claim.reward_id = params[:reward][:id]
+
+    if @claim.save
+      redirect_to project_url(@project), notice: "You have successfully claimed #{@project.claims.description}!"
+    else
+      flash.now[:alert] = @claim.errors.full_messages.first
+      render 'projects/show'
+    end
+  end
+
+  private
+
+  def load_project
+    @project = Project.find(params[:project_id])
+  end
 end
