41b011368ddc74e97f3e6f5fc5795db04fe2f01e
Tony <ANCLIST@GMAIL.COM>
2017-11-01 12:10:12 -0400

    Updated the Test email must be unique on the user test file to use FactoryBot

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,8 +2,8 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   test "email must be unique" do
-    user = User.create(email: "bettymaker@gmail.com", password: "12345678", password_confirmation: "12345678")
-    user2 = User.new(email: "bettymaker@gmail.com", password: "00000000", password_confirmation: "00000000")
+    user = create(:user)
+    user2 = build(:user, email: user.email)
     refute user2.valid?
   end
 
