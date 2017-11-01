493d88e17ab5d008351e19a30af7c3a960deac1f
COREY COSMAN <cosman.corey@gmail.com>
2017-11-01 11:12:35 -0400

    added functionality- only backers can see comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :pledges
   has_many :projects
+  has_many :pledge_projects, through: :pledges, source: :project
   has_many :comments
 
 
