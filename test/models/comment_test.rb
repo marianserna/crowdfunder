8a16ad8ba891bc2f269914453bad7218887bffaa
Marian Serna <marianhalliday@gmail.com>
2017-11-01 11:37:37 -0400

    Adding back old projects

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,16 +1,16 @@
-require 'test_helper'
-
-class CommentTest < ActiveSupport::TestCase
-
-  test 'comment can be created' do
-
-    comment = create(:comment)
-    comment.save
-    assert comment.valid?
-
-  end
-
-
-
-
-end
+# require 'test_helper'
+#
+# class CommentTest < ActiveSupport::TestCase
+#
+#   test 'comment can be created' do
+#
+#     comment = create(:comment)
+#     comment.save
+#     assert comment.valid?
+#
+#   end
+#
+#
+#
+#
+# end
